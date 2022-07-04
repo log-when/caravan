5f6b9aa1c0a05e5f4016dc5ad393846631e7c366
YFNY <13121802177@163.com>
2022-07-04 18:35:31 +0800

    update test

--- src/test/scala/wishbone/SVAtest.scala
+++ src/test/scala/wishbone/SVAtest.scala
@@ -7,11 +7,6 @@ import chiseltest._
 import chiseltest.formal._
 import chisel3._
 import org.scalatest.freespec._
-import org.scalatest.flatspec._
-import org.scalatest.matchers.should._
-import chiseltest._
-import chiseltest.formal._
-import chisel3._
 import chiseltest.formal.svaSeq._
 
 import common.MemoryDumpFileHelper // necessary to import
@@ -23,11 +18,8 @@ class WishboneHostProp(implicit config: WishboneConfig) extends WishboneHost()(c
   val stb_o = io.wbMasterTransmitter.bits.stb 
   val ack_i = io.wbSlaveReceiver.bits.ack
   val err_i = io.wbSlaveReceiver.bits.err
-
-  val w = Wire(Bool())
-  w := true.B
   //it is allowed to get ack in the same cycle with stb
-  svaSeqAnno.makeSVAAnno(this.reset, ap(stb_o) |-> |- ap(stb_o) U ap(ack_i ) -| || G ap(stb_o) )
+  svaSeqAnno.makeSVAAnno(this.reset, ap(stb_o) |-> |- ap(stb_o) U ap(ack_i || err_i) -| || G ap(stb_o) )
 }
 
 class HarnessTest2 extends AnyFlatSpec with ChiselScalatestTester with Formal {
@@ -37,8 +29,5 @@ class HarnessTest2 extends AnyFlatSpec with ChiselScalatestTester with Formal {
   behavior of "WishboneHost"
   it should "pass" in {
     verify(new WishboneHostProp(), Seq(BoundedCheck(100), BtormcEngineAnnotation))
-    //verify(new GCD(), Seq(BoundedCheck(20), BtormcEngineAnnotation) )
-    //verify(new CounterProp1(4), Seq(BoundedCheck(20), BtormcEngineAnnotation))
-    //verify(new CounterProp2(4), Seq(BoundedCheck(20), BtormcEngineAnnotation))
   }
 }
